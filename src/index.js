e5df2d414ec6109ec88b8e10e29dd7b4442d797b
Sandy Zheng <sandyzng105@gmail.com>
2023-01-13 20:59:26 -0500

    reactjs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app'
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3tZrGL3yS85jPpOGw9j2kVJHz3EzNRWU",
+  authDomain: "bumpr-website.firebaseapp.com",
+  projectId: "bumpr-website",
+  storageBucket: "bumpr-website.appspot.com",
+  messagingSenderId: "1031267175504",
+  appId: "1:1031267175504:web:5cace6b5ccdbbde3a39555",
+  measurementId: "G-7WZVSKGPSL"
+};
+
+initializeApp(firebaseConfig)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
